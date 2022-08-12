e2305575bc65c32258689046336e089b0ebce2a4
Aphenphos <MaxwillWinters@gmail.com>
2022-08-12 09:07:34 -0400

    forgot to setup github oauth, still not properly redirecting though

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,10 +1,17 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
 drop table if exists github_users;
+drop table if exists posts;
 
 create table github_users (
     id bigint generated always as identity primary key,
     username text not null,
     email text,
     avatar text
+);
+
+create table posts (
+    id bigint generated always as identity primary key,
+    user_id bigint not null,
+    content text
 )
--- sql/setup.test.sql
+++ sql/setup.test.sql
@@ -6,3 +6,10 @@ insert into github_users (
 values
 ('geg', 'geg@geg.com', 'geg.jpg')
 ('deg', 'deg@deg.com', 'deg.jpg')
+
+insert into posts (
+    content
+)
+values
+('1', 'wow is this dope'),
+('2', 'woah');
